06b87d4fc86d70bc24d684d115f8ba1c1f5d52a5
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-22 15:03:32 -0700

    added the function to create and add buttons to display'

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,28 @@
-alert('test');
+let topics = [
+  "music",
+  "Black Sabbath",
+  "Blue Oyster Cult",
+  "Led Zeppelin",
+  "slipknot",
+  "Metallica"
+];
+
+$(document).ready(function() {
+  createButtonsFromArray(topics);
+});
+
+var createButtonsFromArray = function(arr) {
+    var rowCreaterIndex = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (i === 0 || i % 12 === 0) {
+      $(".container").append(`<div class='row' id= 'row-${i}'></div>`);
+      $(`#row-${i}`).append(
+        `<div class='col'><button type='button' class='btn btn-primary'>${arr[i]}</button></div>`
+      );
+      rowCreaterIndex = i;
+    } else {
+        $(`#row-${rowCreaterIndex}`).append(
+            `<div class='col'><button type='button' class='btn btn-primary'>${arr[i]}</button></div>`
+          );    }
+  }
+};
