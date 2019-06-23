9a4ff7cc5c229a54729e32a1148526ae9cbda9e7
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-22 17:19:59 -0700

    added functionality for gifs to dispaly and when clicked, they move

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,20 +9,103 @@ let topics = [
 
 $(document).ready(function() {
   createButtonsFromArray(topics);
+  queriesTheInputAndDisplaysGifsToDisplay("metallica");
 });
 
+/*
+  function takes in an array and from that array, creates buttons to display on the
+  display
+*/
 var createButtonsFromArray = function(arr) {
-    var rowCreaterIndex = 0;
+  var rowCreaterIndex = 0;
   for (var i = 0; i < arr.length; i++) {
     if (i === 0 || i % 12 === 0) {
-      $(".container").append(`<div class='row' id= 'row-${i}'></div>`);
+      $(".container").prepend(`<div class='row' id= 'row-${i}'></div>`);
       $(`#row-${i}`).append(
-        `<div class='col'><button type='button' class='btn btn-primary'>${arr[i]}</button></div>`
+        `<div class='col'><button type='button' class='gifButton btn btn-primary'>${
+          arr[i]
+        }</button></div>`
       );
       rowCreaterIndex = i;
     } else {
-        $(`#row-${rowCreaterIndex}`).append(
-            `<div class='col'><button type='button' class='btn btn-primary'>${arr[i]}</button></div>`
-          );    }
+      $(`#row-${rowCreaterIndex}`).append(
+        `<div class='col'><button type='button' class=' gifButton btn btn-primary'>${
+          arr[i]
+        }</button></div>`
+      );
+    }
   }
 };
+
+var queriesTheInputAndDisplaysGifsToDisplay = function(stringToQuery) {
+  var APIKEY = "mnC6R0UR5Gi6CHuOXic2w54GdnD5T0Qi";
+  var gifArray = [];
+
+  axios({
+    url: `http://api.giphy.com/v1/gifs/search?q=${stringToQuery}&api_key=${APIKEY}&limit=6`,
+    method: "GET"
+  })
+    .then(function(response) {
+      console.log(response);
+      gifArray = response.data.data;
+      var newRow;
+      var newCardDeck;
+
+      for (var i = 0; i < gifArray.length; i++) {
+        if (i === 0 || i % 3 === 0) {
+          if (i % 3 === 0 && i !== 0) {
+            newRow.append(newCardDeck);
+            $("#gifAndSearch").prepend(newRow);
+            console.log("mega if");
+          }
+          newRow = $("<div class='row'></div>");
+          newCardDeck = $("<div class='card-deck'></div>");
+          console.log("if");
+          newCardDeck.append(
+            `<div class='card'><div class='card-body'><h5 class='card-title'>Rating: ${
+              gifArray[i].rating
+            }</h5><img class= "card-img-top" animated='false' gifyId="${gifArray[i].id}" src="${
+              gifArray[i].images["480w_still"].url
+            }" height="268px" width="246px" ></div></div>`
+          );
+        } else {
+          console.log("else");
+          newCardDeck.append(
+            `<div class='card'><div class='card-body'><h5 class='card-title'>Rating: ${
+              gifArray[i].rating
+            }</h5><img class="card-img-top" animated='false' gifyId="${gifArray[i].id}" src="${
+              gifArray[i].images["480w_still"].url
+            }" height="268px" width="246px"></div></div>`
+          );
+        }
+      }
+      newRow.append(newCardDeck);
+
+      $("#gifAndSearch").prepend(newRow);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+};
+
+$(document.body).on("click", "img", function(e) {
+  var APIKEY = "mnC6R0UR5Gi6CHuOXic2w54GdnD5T0Qi";
+  var gifyId = $(this).attr("gifyid");
+  axios({
+    url: `https://api.giphy.com/v1/gifs/${gifyId}?api_key=${APIKEY}`,
+    method: "GET"
+  }).then(function(response) {
+
+    if ($(e.currentTarget).attr("animated") === "false") {
+
+      $(e.currentTarget).attr("src", `${response.data.data.images.original.url}`);
+      $(e.currentTarget).attr("animated", "true")
+    } else if ($(e.currentTarget).attr("animated") === "true") {
+      $(e.currentTarget).attr("src", `${response.data.data.images["480w_still"].url}`);
+      $(e.currentTarget).attr("animated", "false")
+
+    }
+  });
+});
+
+$(document.body).on("click", ".gifButton", function() {});
